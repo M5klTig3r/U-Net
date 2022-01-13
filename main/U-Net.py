4cbacdec9314c5e18dd41e54f79ee46ac6823eac
Andrea Matécsa <andrea.matecsa@gmail.com>
2022-01-13 17:45:05 +0100

    UNet update
    Added Cuda and epochs with better output.

--- main/U-Net.py
+++ main/U-Net.py
@@ -12,11 +12,11 @@ from Decoder import Decoder
 
 class UNet(nn.Module):
     def __init__(self, enc_chs=(1, 64, 128, 256, 512, 1024), dec_chs=(1024, 512, 256, 128, 64), num_class=1,
-                 retain_dim=False, out_sz=(572, 572)):
+                 retain_dim=False):
         super().__init__()
         self.encoder = Encoder(enc_chs)
         self.decoder = Decoder(dec_chs)
-        self.head = nn.Conv2d(dec_chs[-1], num_class, 1)
+        self.head = nn.Conv2d(dec_chs[-1], num_class, (1, 1))
         self.retain_dim = retain_dim
 
     def forward(self, x):
@@ -35,8 +35,7 @@ def sample_image(n_row, batches_done, current_epoch, real_images):
 
 dataloader = torch.utils.data.DataLoader(datasets.ImageFolder(
     # "../../iOCT/bigVol_9mm",
-    "../../PMSD/ImageDenoising(Averaging)Cubes/sorted/cut_eye_no_needle/86271bd2-31fb-436f-9e31-9ec5a3a4f7648203"
-    "/bigVol_9mm",
+    "../../iOCT/bigVol_9mm",
     transform=transforms.Compose(
         [transforms.Grayscale(num_output_channels=1),
          transforms.Resize((512, 512)),
@@ -48,20 +47,28 @@ dataloader = torch.utils.data.DataLoader(datasets.ImageFolder(
     shuffle=True,
 )
 
-encoder = Encoder()
+cuda = True if torch.cuda.is_available() else False
+torch.cuda.empty_cache()
+unet = UNet()
+
+FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
+if cuda:
+    print(f"Cuda: {cuda}")
+    unet.cuda()
+
 # input image
-for epoch in range(10):
+for epoch in range(300):
     for i, (images, labels) in enumerate(dataloader):
 
         x = Variable(images.type(FloatTensor))
 
-        unet = UNet()
-        unet(x).shape
+        output = unet(x)
 
         print(
-            "[Epoch %d/%d] [Batch %d/%d]"
-            % (0, 10, i, len(dataloader))
+            f"[Epoch %d/%d] [Batch %d/%d] [Image size {output.shape}]"
+            % (epoch, 300, i, len(dataloader))
         )
         batches_done = epoch * len(dataloader) + i
-        if batches_done % 50 == 0:
+        if batches_done % 100 == 0:
             sample_image(n_row=10, batches_done=batches_done, current_epoch=epoch, real_images=x)
